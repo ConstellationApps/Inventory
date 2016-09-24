5d8fb58646d6fe04e68187ce89e5bb2592e6a4cc
Michael Aldridge <aldridge.mac@gmail.com>
2016-09-24 01:58:26 -0500

    Return items in index view

--- orderboard/models.py
+++ orderboard/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
+    LIFECYCLE_STAGES=[('W', "Wanted"),
+                      ('O', "Ordered"),
+                      ('R', "Recieved")]
+
     name = models.CharField(max_length=128)
     quantity = models.IntegerField()
     notes = models.TextField()
+    stage = models.CharField(max_length=1, choices=LIFECYCLE_STAGES, default='W')
--- orderboard/views.py
+++ orderboard/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .forms import ItemForm
-
-# Create your views here.
+from .models import Item
 
 def index(request):
     ItemFormSet = formset_factory(ItemForm)
@@ -14,7 +13,22 @@ def index(request):
             for form in formset:
                 myobject = form.save()
     formset = ItemFormSet()
-    return render(request, 'orderboard/index.html', {'formset': formset})
+
+    allCards = []
+    for phase in Item.LIFECYCLE_STAGES:
+        phaseDict = {}
+        phaseDict['name'] = phase[1]
+        phaseDict['cards'] = []
+        for item in Item.objects.filter(stage=phase[0]):
+            cardDict = {}
+            cardDict['id'] = item.pk
+            cardDict['name'] = item.name
+            cardDict['quantity'] = item.quantity
+            cardDict['notes'] = item.notes
+            phaseDict['cards'].append(cardDict)
+        allCards.append(phaseDict)
+
+    return render(request, 'orderboard/index.html', {'formset': formset, 'stage_list':allCards})
 
 
 def new_item(request):
