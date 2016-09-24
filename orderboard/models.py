22cc557d7f71a0afbb66e203911b92174a529f39
Michael Aldridge <aldridge.mac@gmail.com>
2016-09-24 02:07:29 -0500

    Add Archived state and default quantity

--- orderboard/models.py
+++ orderboard/models.py
@@ -5,9 +5,10 @@ from django.db import models
 class Item(models.Model):
     LIFECYCLE_STAGES=[('W', "Wanted"),
                       ('O', "Ordered"),
-                      ('R', "Recieved")]
+                      ('R', "Recieved"),
+                      ('A', "Archived")]
 
     name = models.CharField(max_length=128)
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(default=1)
     notes = models.TextField()
     stage = models.CharField(max_length=1, choices=LIFECYCLE_STAGES, default='W')
