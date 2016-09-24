36e9cac6a5bc386ea28bc50c424bf6efe8089337
Michael Aldridge <aldridge.mac@gmail.com>
2016-09-24 00:54:23 -0500

    Define Item ModelForm

--- orderboard/views.py
+++ orderboard/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import ItemForm
+
 # Create your views here.
 
 def index(request):
     return HttpResponse("We're Live!")
+
+def new_item(request):
+    form = ItemForm()
+    return HttpResponse(form)
