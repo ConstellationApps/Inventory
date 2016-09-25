a00e463d1cbd5b690aac24449729a45405912205
Michael Aldridge <aldridge.mac@gmail.com>
2016-09-25 18:53:22 -0500

    Change default state to 'Wanted'

--- orderboard/models.py
+++ orderboard/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
-    LIFECYCLE_STAGES=[('W', "Wanted"),
+    LIFECYCLE_STAGES=[('P', 'Proposed'),
+                      ('W', "Wanted"),
                       ('O', "Ordered"),
                       ('R', "Recieved"),
                       ('A', "Archived")]
@@ -11,4 +12,4 @@ class Item(models.Model):
     name = models.CharField(max_length=128)
     quantity = models.IntegerField(default=1)
     notes = models.TextField()
-    stage = models.CharField(max_length=1, choices=LIFECYCLE_STAGES, default='W')
+    stage = models.CharField(max_length=1, choices=LIFECYCLE_STAGES, default='P')
