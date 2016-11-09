5f36913531a7f8b593cec179ef0ce4355802eb39
Michael Aldridge <aldridge.mac@gmail.com>
2016-11-09 16:33:54 -0600

    Add forms for Board and Stage

--- forms.py
+++ forms.py
@@ -1,7 +1,19 @@
 from .models import Item
+from .models import Board
+from .models import Stage
 from django.forms import ModelForm
 
 class ItemForm(ModelForm):
     class Meta:
         model = Item
         fields = ['name', 'quantity', 'notes']
+
+class BoardForm(ModelForm):
+    class Meta:
+        model = Board
+        fields = ['name', 'desc']
+
+class StageForm(ModelForm):
+    class Meta:
+        model = Stage
+        fields = ['name', 'index', 'active']
