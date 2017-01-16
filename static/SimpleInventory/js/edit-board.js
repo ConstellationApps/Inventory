07102c6cfb61d74c77fcf672a51ba57821a67ede
DangerOnTheRanger <tuxfreak@tuxfamily.org>
2017-01-15 23:07:02 -0600

    edit-board.js uses window.location.href now

--- static/SimpleInventory/js/edit-board.js
+++ static/SimpleInventory/js/edit-board.js
@@ -5,11 +5,11 @@ function redirect(event) {
   event.preventDefault();
   var form_data = $('#editBoardForm');
     $.post(event.target.action, form_data.serialize(), function(response) {
-	window.location.assign("/view/list");
+	window.location.href = "/view/list";
   }, 'json')
     .fail(function(jqXHR) {
       if (jqXHR.status == 400 || jqXHR.status == 500) {
-        message.MaterialSnackbar.showSnackbar({message: jqXHR.responseText});
+        message.MaterialSnackbar.showSnackbar({message: "An error occurred while submitting the form."});
       } else {
         message.MaterialSnackbar.showSnackbar({message: jqXHR.responseText});
       }
